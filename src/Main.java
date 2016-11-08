aab116b192493feedb9cdf671ccdc914198540b1
Sean Rodriguez <sean.rodriguez@outlook.com>
2016-11-08 15:39:14 -0500

    Implemented Main class to use threads
    Main class uses threads to read and write data to/from files concurrently.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,87 @@
-import javax.swing.JOptionPane;
+import javax.swing.*;
+import java.io.*;
+import java.util.Scanner;
 
 /**
- * Project Name: Registration
- * Purpose:
+ * Project Name: Registration<br>
+ * File Name: Main.java<br>
+ * Purpose: Uses threads to write and read Students to/from a file concurrently
  *
- * @author Sean Rodriguez <sean.rodriguez@outlook.com>
- * @since October 21, 2016
- * @version 1.0
- *
+ * @author Sean Rodriguez
+ * @since 1.0
+ * @version %I%, %G%
  */
 
-class Main {
-    public static void main(String[] args) throws MyInvalidDateException {
+public class Main {
+    public static void main(String[] args) throws IOException, MyInvalidDateException {
+
+        // Prompt user for file names to write to and read from
+//        String filename1 = JOptionPane.showInputDialog(
+//                "Choose a file to write to:");
+//        String filename1 = JOptionPane.showInputDialog(
+//                "Choose a file to write to:");
+//        String filename1 = JOptionPane.showInputDialog(
+//                "Choose a file to write to:");
+
+        String filename1 = "cs111.txt";
+        String filename2 = "cs211.txt";
+        String filename3 = "cs212.txt";
+
+
+        // Create three write Threads that pass in a filename to write to
+        WriteThread wThread1 = new WriteThread(filename1);
+        WriteThread wThread2 = new WriteThread(filename2);
+        WriteThread wThread3 = new WriteThread(filename3);
+
+        // Start threads
+        wThread1.start();
+        wThread2.start();
+        wThread3.start();
+
+        try {
+            // Wait for all threads to finish
+            wThread1.join();
+            wThread2.join();
+            wThread3.join();
+        } catch(InterruptedException e) {   // Catches interruption errors inside the threads
+            System.out.println("\nERROR: System failed to update file.");
+        }
+
+        // Print write threads status'
+        System.out.println("\nFile update completed.\n");
+
+        // Create three read Thread that pass in a filename to read from
+        Thread rThread1 = new Thread(new ReadRunnable(filename1));
+        Thread rThread2 = new Thread(new ReadRunnable(filename2));
+        Thread rThread3 = new Thread(new ReadRunnable(filename3));
+
+        // Start threads
+        rThread1.start();
+        rThread2.start();
+        rThread3.start();
+
+        try {
+            // Wait for all threads to finish
+            rThread1.join();
+            rThread2.join();
+            rThread3.join();
+        } catch(InterruptedException e) {   // Catches interruption errors inside the threads
+            System.out.println("ERROR: System failed to read file.");
+        }
+
+        System.out.println("\nNumber of Students: " +
+                QueensCollegeStudent.getNumberOfStudents());    // Prints number of total student
+        System.out.println("File read successfully");           // Prints threads status'
+    }
+
+
+    /**
+     * Writes to the file using randomly selected pre-determined records
+     * @param filename file address to write to
+     * @throws IOException catches all IO errors from FileWrite object
+     */
+    static void writeDataToFile(String filename) throws IOException {
+        FileWriter outFile = new FileWriter(filename, true);    // Create FileWriter to open file
 
         // Pre-determined information for first name...
         String[] fname = {
@@ -35,7 +105,7 @@ class Main {
         };
 
         // ...GPA...
-        double[] gp = {
+        double[] gpa = {
                 3.30, 3.09, 2.85, 2.99, 3.01, 3.24,
                 2.14, 3.78, 3.26, 2.45
         };
@@ -64,35 +134,55 @@ class Main {
         int n = Integer.parseInt(JOptionPane.showInputDialog(
                 "How many students do you want to initiate?"));
 
-        // Create an array of the number of students the user wants
-        QueensCollegeStudent[] students = new QueensCollegeStudent[n];
-
-        // Iterate through array, populate values for each student
-        // Using randomly pre-determined values
-        for(int i = 0; i < students.length; i++) {
-            int random = (int)(Math.random() * 10);
-            students[i] = new QueensCollegeStudent(fname[random], lname[random],
-                    id[random], gp[random], year[random], month[random], day[random],
-                    vl[random]);
+        // Write 'n' randomly generated students to file
+        for(int i = 0; i < n; i++) {
+            int rand = (int)(Math.random() * 10);   // Generate random index
+            outFile.write(fname[rand] + " " + lname[rand] + " " + id[rand] + " "
+                    + gpa[rand] + " " + year[rand] + " " + month[rand] + " "
+                    + day[rand] + " " + vl[rand] + "\n");
         }
 
-        System.out.println();
+        outFile.flush();    // flush FileWriter
+        outFile.close();    // close FileWriter
+    }
 
-        // Iterate through array, call displayAttributes to display student's info
-        for(QueensCollegeStudent student : students) {
-            student.displayAttributes();
-            System.out.println();
+    /**
+     * Read from file, prints file average GPA
+     * @param filename file address to read from
+     * @return average GPA of all students in file
+     * @throws FileNotFoundException catches 'file not found' error from File object
+     * @throws MyInvalidDateException catches invalid dates from Date object
+     */
+    static double readDataFromFile(String filename) throws FileNotFoundException, MyInvalidDateException {
+        File file = new File(filename);         // Opens filename argument
+        Scanner inFile = new Scanner(file);     // Scanner to iterate through the file
+
+        // Variables to calculate gpa return
+        int count = 0;
+        double totalGPA = 0;
+
+        // Loop until end of file
+        while(inFile.hasNext()) {
+
+            // Grab all variables for student record
+            String firstName = inFile.next();
+            String lastName = inFile.next();
+            int id = inFile.nextInt();
+            double gpa = inFile.nextDouble();
+            int year = inFile.nextInt();
+            int month = inFile.nextInt();
+            int day = inFile.nextInt();
+            String venusLogin = inFile.next();
+
+            // Instantiate student object
+            QueensCollegeStudent qcStudent = new QueensCollegeStudent(firstName, lastName, id,
+                    gpa, year, month, day, venusLogin);
+
+            count++;            // Increment count by 1
+            totalGPA += gpa;    // Add GPAs to total GPA
         }
 
-        // Iterate through array, call isValidVenusLogin
-        // To display if each student's CUNYFirst ID is valid
-        for(QueensCollegeStudent student : students)
-            System.out.println(student.firstName + " " +
-                    student.lastName + "'s Venus Login is " +
-                    (student.isValidVenusLogin() ? "Valid" : "Invalid"));
-
-        System.out.println("\nThe number of students that exists now is " +
-                QueensCollegeStudent.getNumberOfStudents());
-        System.out.println();
+        inFile.close();             // close Scanner
+        return totalGPA / count;    // return average gpa of file
     }
 }
